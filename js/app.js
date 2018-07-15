3b72277b92bd1749a4b821350881ee59d5304019
Dine <ariadne0288@hotmail.com>
2018-07-15 20:26:25 -0300

    Cards alunas finalizado

--- js/app.js
+++ js/app.js
@@ -40,26 +40,41 @@ function showHideTabs(e) {
   }
 }
 
-var studentPhoto
-var studentName
-var studentOffice
-var studentClass
-var studentStatus
-var techMedia
-var softMedia
+var studentPhoto;
+var studentName;
+var studentOffice;
+var studentClass;
+var studentStatus;
+var techMedia;
+var softMedia;
 
 function showStudentsCards() {
-  for (office in data) {
+  for (var office in data) {
     studentOffice = office;
-    for (_class in data[office]) {
-      studentClass = _class;
-      for (i in data[office][_class]['students']) {
-        studentName = data[office][_class]['students'][i]['name'];
-        studentPhoto = data[office][_class]['students'][i]['photo'];
-        studentStatus = data[office][_class]['students'][i]['active'];
-        techMedia = data[office][_class]['students'][i]['sprints'][1]['tech']
-        techMedia = data[office][_class]['students'][i]['sprints'][1]['hse']
-        createStudentCard()
+    for (var classes in data[office]) {
+      studentClass = classes;
+      for (var i in data[office][classes]['students']) {
+        studentName = data[office][classes]['students'][i]['name'];
+        studentPhoto = data[office][classes]['students'][i]['photo'];
+        if (data[office][classes]['students'][i]['active'] === true) {
+          studentStatus = 'Ativa';
+        } else {
+          studentStatus = 'Inativa';
+        }
+        var sprint = data[office][classes]['students'][i]['sprints'];
+        var sprintLength = sprint.length;
+        techMedia = 0;
+        for (var j = 0; j < sprintLength; j++) {
+          techMedia += sprint[j]['score']['tech'];
+        }
+        techMedia = parseInt(techMedia/sprintLength);
+
+        softMedia = 0;
+        for (var j = 0; j < sprintLength; j++) {
+          softMedia += sprint[j]['score']['hse'];
+        }
+        softMedia = parseInt(softMedia/sprintLength);
+        createStudentCard();
       }
     }
   }
